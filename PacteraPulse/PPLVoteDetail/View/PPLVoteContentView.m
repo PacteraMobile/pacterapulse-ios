f4b33e6cf6b2c22c24d097ba0090f2f9065b2939
Martin Xiao <martin.xiao@pactera.com.au>
2015-05-19 12:23:07 +1000

    Add PPLVoteDetailTest cases, PPLVoteDetailViewContoller

---
+++ PacteraPulse/PPLVoteDetail/View/PPLVoteContentView.m
@@ -0,0 +1,21 @@
+//
+//  PPLVoteContentView.m
+//  PacteraPulse
+//
+//  Created by jin on 19/05/2015.
+//  Copyright (c) 2015 Pactera. All rights reserved.
+//
+
+#import "PPLVoteContentView.h"
+
+@implementation PPLVoteContentView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
