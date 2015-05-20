40fc91d6a1b3ae0911d4d6f11f2bd52680f98e0d
Martin Xiao <martin.xiao@pactera.com.au>
2015-05-20 16:23:51 +1000

    Update voteDetail themeColor

--- PacteraPulse/PPLVoteDetail/View/PPLVoteContentView.m
+++ PacteraPulse/PPLVoteDetail/View/PPLVoteContentView.m
@@ -7,14 +7,17 @@
 //
 
 #import "PPLVoteContentView.h"
+
 @interface PPLVoteContentView () {
   CGFloat emotionMaxY;
   NSArray *questionList;
 }
 @property(nonatomic, strong) UILabel *greetingLalel;
 @property(nonatomic, strong) UIImageView *emotionView;
+@property(nonatomic, strong) UIButton *submitButton;
 @property(nonatomic, strong) PPLVoteContentQuestionView *workQuestionView;
-@property(nonatomic, strong) PPLVoteContentQuestionView *communicationQuestionView;
+@property(nonatomic, strong)
+    PPLVoteContentQuestionView *communicationQuestionView;
 @property(nonatomic, strong) PPLVoteContentQuestionView *projectQuestionView;
 
 @end
@@ -75,7 +78,8 @@ NSString *const kMainTitleContent = @"We would love to hear more";
 }
 
 - (void)contentWithEmotionGreeting {
-  self.emotionView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"happyIcon"]];
+  self.emotionView =
+      [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"happyIcon"]];
   self.emotionView.contentMode = UIViewContentModeScaleAspectFit;
   self.emotionView.clipsToBounds = YES;
   CGFloat emotionPaddingTop = kPaddingTen;
@@ -121,32 +125,29 @@ NSString *const kMainTitleContent = @"We would love to hear more";
       3;
 
   CGFloat mainTitleMaxY = CGRectGetMaxY(mainTitleLabel.frame);
-  self.workQuestionView = [
-      [PPLVoteContentQuestionView alloc]
+  self.workQuestionView = [[PPLVoteContentQuestionView alloc]
       initContentQuestionViewWithFrame:CGRectMake(0, mainTitleMaxY,
                                                   CGRectGetWidth(self.frame),
                                                   heightForEachQueation)
-                      withLabelContent:questionList[0] withFeedBackType:kUnHappyIcon];
+                      withLabelContent:questionList[0]];
   [self addSubview:self.workQuestionView];
 
   CGFloat workQueationViewMaxY = CGRectGetMaxY(self.workQuestionView.frame);
-  self.communicationQuestionView = [
-      [PPLVoteContentQuestionView alloc]
+  self.communicationQuestionView = [[PPLVoteContentQuestionView alloc]
       initContentQuestionViewWithFrame:CGRectMake(0, workQueationViewMaxY,
                                                   CGRectGetWidth(self.frame),
                                                   heightForEachQueation)
-                      withLabelContent:questionList[1] withFeedBackType:kUnHappyIcon];
+                      withLabelContent:questionList[1]];
   [self addSubview:self.communicationQuestionView];
 
   CGFloat communicationQueationViewMaxY =
       CGRectGetMaxY(self.communicationQuestionView.frame);
-  self.projectQuestionView =
-      [[PPLVoteContentQuestionView alloc]
-          initContentQuestionViewWithFrame:CGRectMake(
-                                               0, communicationQueationViewMaxY,
-                                               CGRectGetWidth(self.frame),
-                                               heightForEachQueation)
-                          withLabelContent:questionList[2] withFeedBackType:kUnHappyIcon];
+  self.projectQuestionView = [[PPLVoteContentQuestionView alloc]
+      initContentQuestionViewWithFrame:CGRectMake(0,
+                                                  communicationQueationViewMaxY,
+                                                  CGRectGetWidth(self.frame),
+                                                  heightForEachQueation)
+                      withLabelContent:questionList[2]];
   [self addSubview:self.projectQuestionView];
 }
 
@@ -159,10 +160,10 @@ NSString *const kMainTitleContent = @"We would love to hear more";
   CGRect submitButtonFrame =
       CGRectMake(submitButtonPaddingLeftRight, submitButtonY, submitButtonWidth,
                  kSubmitButtonHeigth);
-  UIButton *submitButton = [[UIButton alloc] initWithFrame:submitButtonFrame];
-  [submitButton setTitle:kSubmitButton forState:UIControlStateNormal];
-  submitButton.backgroundColor = [UIColor redColor];
-  [self addSubview:submitButton];
+  self.submitButton = [[UIButton alloc] initWithFrame:submitButtonFrame];
+  [self.submitButton setTitle:kSubmitButton forState:UIControlStateNormal];
+  self.submitButton.backgroundColor = [UIColor redColor];
+  [self addSubview:self.submitButton];
 }
 
 - (void)setUserName:(NSString *)userName {
@@ -170,28 +171,55 @@ NSString *const kMainTitleContent = @"We would love to hear more";
       setText:[NSString stringWithFormat:@"Thank you %@", userName]];
 }
 
-- (void)setFeedBack:(FeedBackType)feedBack{
+- (void)setFeedBack:(FeedBackType)feedBack {
   NSString *imageName = @"";
   switch (feedBack) {
   case kHappyIcon:
     imageName = @"happyIcon";
+    [self.submitButton setBackgroundColor:HAPPY_COLOR];
+    [self.submitButton setTitleColor:HAPPY_FONT_COLOR
+                            forState:UIControlStateNormal];
+      self.projectQuestionView.feedBack = kHappyIcon;
+      self.communicationQuestionView.feedBack = kHappyIcon;
+      self.workQuestionView.feedBack = kHappyIcon;
     break;
   case kSosoIcon:
     imageName = @"sosoIcon";
+    [self.submitButton setBackgroundColor:SOSO_COLOR];
+    [self.submitButton setTitleColor:SOSO_FONT_COLOR
+                            forState:UIControlStateNormal];
+      self.projectQuestionView.feedBack = kSosoIcon;
+      self.communicationQuestionView.feedBack = kSosoIcon;
+      self.workQuestionView.feedBack = kSosoIcon;
     break;
   case kUnHappyIcon:
     imageName = @"unhappyIcon";
+    [self.submitButton setBackgroundColor:UNHAPPY_COLOR];
+    [self.submitButton setTitleColor:UNHAPPY_FONT_COLOR
+                            forState:UIControlStateNormal];
+      self.projectQuestionView.feedBack = kUnHappyIcon;
+      self.communicationQuestionView.feedBack = kUnHappyIcon;
+      self.workQuestionView.feedBack = kUnHappyIcon;
     break;
   }
   [self.emotionView setImage:[UIImage imageNamed:imageName]];
 }
 
-- (NSArray*)fetchVoteComments{
+- (NSArray *)fetchVoteComments {
   NSMutableArray *voteComments = [NSMutableArray array];
-  [voteComments addObject:[NSString stringWithFormat:@"%f", self.workQuestionView.contentSilderView.value]];
-  [voteComments addObject:[NSString stringWithFormat:@"%f", self.communicationQuestionView.contentSilderView.value]];
-  [voteComments addObject:[NSString stringWithFormat:@"%f", self.projectQuestionView.contentSilderView.value]];
-  
+  [voteComments
+      addObject:[NSString
+                    stringWithFormat:@"%f", self.workQuestionView
+                                                .contentSilderView.value]];
+  [voteComments
+      addObject:[NSString
+                    stringWithFormat:@"%f", self.communicationQuestionView
+                                                .contentSilderView.value]];
+  [voteComments
+      addObject:[NSString
+                    stringWithFormat:@"%f", self.projectQuestionView
+                                                .contentSilderView.value]];
+
   return voteComments;
 }
 
