2a9d3d638097304d62913b27f323c3bdb82db75d
Martin Xiao <martin.xiao@pactera.com.au>
2015-05-21 16:39:21 +1000

    update PPLVoteDetailTests testCases

--- PacteraPulseTests/PPLVoteDetailTests/PPLVoteDetailTests.m
+++ PacteraPulseTests/PPLVoteDetailTests/PPLVoteDetailTests.m
@@ -10,15 +10,21 @@
 #import <XCTest/XCTest.h>
 #import "PPLVoteContentView.h"
 #import "PPLVoteContentQuestionView.h"
+#import "PPLVoteDetailViewController.h"
+#import "PPLVoteData.h"
 #import "PPLUtils.h"
+#import <OCMock/OCMock.h>
 @interface PPLVoteDetailTests : XCTestCase
-
+@property(nonatomic, strong) PPLVoteContentView *testVoteContentView;
+@property(nonatomic, strong) PPLVoteDetailViewController *pplVoteViewController;
 @end
 
 @implementation PPLVoteDetailTests
 
 - (void)setUp {
   [super setUp];
+  self.testVoteContentView =
+      [[PPLVoteContentView alloc] initViewWithFrame:CGRectMake(0, 0, 400, 650)];
   // Put setup code here. This method is called before the invocation of each
   // test method in the class.
 }
@@ -29,18 +35,6 @@
   [super tearDown];
 }
 
-- (void)testExample {
-  // This is an example of a functional test case.
-  XCTAssert(YES, @"Pass");
-}
-
-- (void)testPerformanceExample {
-  // This is an example of a performance test case.
-  [self measureBlock:^{
-      // Put the code you want to measure the time of here.
-  }];
-}
-
 - (void)testVoteContentQuestionView {
 
   PPLVoteContentQuestionView *questionView = [[PPLVoteContentQuestionView alloc]
@@ -49,15 +43,17 @@
   XCTAssertNotNil(questionView, @"it should init a queationView successfully");
 }
 
-- (void)testVoteContentView {
-  PPLVoteContentView *testVoteContentView =
-      [[PPLVoteContentView alloc] initViewWithFrame:CGRectMake(0, 0, 400, 650)];
-  [testVoteContentView setFeedBack:kSoso];
-  [testVoteContentView setUserName:@"Randy"];
-  XCTAssertEqual(3, [[testVoteContentView fetchVoteComments] count],
-                 @"it should have three comments");
+- (void)testVoteContent {
 
-  NSArray *testSubViews = testVoteContentView.subviews;
+  [self.testVoteContentView setFeedBack:kSoso];
+  [self.testVoteContentView setUserName:@"Randy"];
+  XCTAssertEqual(3, [[self.testVoteContentView fetchVoteComments] count],
+                 @"it should have three comments");
+}
+
+- (void)testVoteSlideView {
+
+  NSArray *testSubViews = self.testVoteContentView.subviews;
   for (UIView *view in testSubViews) {
     NSString *subviewClass = [NSString stringWithFormat:@"%@", [view class]];
     NSString *slideClass = [NSString stringWithFormat:@"%@", [UISlider class]];
@@ -67,6 +63,45 @@
                      @"the slide value should be equal to 0.5");
     }
   }
+}
+
+- (void)testVoteDetailViewController {
+  self.pplVoteViewController = [[PPLVoteDetailViewController alloc]
+      initWithNibName:@"PPLVoteDetailViewController"
+               bundle:nil];
+
+  XCTAssertNotNil(self.pplVoteViewController,
+                  @"It should initial a PPLVoteDetailViewController object");
+}
+
+- (void)testSetFeedBackTypeForVoteDetailViewController {
+  [self.pplVoteViewController setFeedBack:kHappy];
+  NSArray *testSubViews = self.pplVoteViewController.view.subviews;
+  for (UIView *view in testSubViews) {
+    if ([view isKindOfClass:[UIImageView class]]) {
+      UIImageView *iconImageView = (UIImageView *)view;
+      XCTAssertNotNil(
+          iconImageView.image,
+          @"It should have a emotion image in the voteDetailViewController");
+    }
+  }
+}
+
+- (void)testVoteData {
+  PPLVoteData *voteData = [PPLVoteData shareInstance];
+  [voteData setDeviceID:[[PPLUtils sharedInstance] getUniqueId]];
+  [voteData setUserID:@"myName"];
+  id voteDataMock = [OCMockObject partialMockForObject:voteData];
+  [[[voteDataMock stub] andDo:^(NSInvocation *invocation) {
+    void (^sendFeedBackResponse)(BOOL status, NSString *serverResponse,
+                                 NSError *error);
+    [invocation getArgument:&sendFeedBackResponse atIndex:3];
+    BOOL status = YES;
+    NSString *serverResponse = @"";
+    NSError *error = nil;
+    sendFeedBackResponse(status, serverResponse, error);
+    XCTAssert(YES, @"Pass");
+  }] sendFeedback:[OCMArg any]];
 }
 
 @end
