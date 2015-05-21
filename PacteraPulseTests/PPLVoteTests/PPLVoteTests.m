e859cf385436de22419dc40276eecceede843620
Martin Xiao <martin.xiao@pactera.com.au>
2015-05-21 11:19:28 +1000

    delete old vote service test case

--- PacteraPulseTests/PPLVoteTests/PPLVoteTests.m
+++ PacteraPulseTests/PPLVoteTests/PPLVoteTests.m
@@ -15,30 +15,32 @@
 
 @implementation PPLVoteTests
 
-- (void)setUp
-{
-    [super setUp];
-    // Put setup code here. This method is called before the invocation of each test method in the class.
+- (void)setUp {
+  [super setUp];
+  // Put setup code here. This method is called before the invocation of each
+  // test method in the class.
 }
 
-- (void)tearDown
-{
-    // Put teardown code here. This method is called after the invocation of each test method in the class.
-    [super tearDown];
+- (void)tearDown {
+  // Put teardown code here. This method is called after the invocation of each
+  // test method in the class.
+  [super tearDown];
 }
 
-- (void)testFeedbackSubmitted
-{
-    PPLVoteData *dataModel = [PPLVoteData shareInstance];
-    XCTestExpectation *completionExpectation = [self expectationWithDescription:@"Checkin Submitted Fetch"];
-    
-    [dataModel sendFeedback:[NSString stringWithFormat:@"%d", (rand()%3 +1 )]
-                   callBack:^(BOOL status, NSString* serverResponse, NSError *error)
-     {
-         XCTAssertTrue(status);
-         [completionExpectation fulfill];
-     }];
-    [self waitForExpectationsWithTimeout:5.0 handler:nil];
+- (void)testFeedbackSubmitted {
+  //    PPLVoteData *dataModel = [PPLVoteData shareInstance];
+  //    XCTestExpectation *completionExpectation = [self
+  //    expectationWithDescription:@"Checkin Submitted Fetch"];
+  //
+  //    [dataModel sendFeedback:[NSString stringWithFormat:@"%d", (rand()%3 +1
+  //    )]
+  //                   callBack:^(BOOL status, NSString* serverResponse, NSError
+  //                   *error)
+  //     {
+  //         XCTAssertTrue(status);
+  //         [completionExpectation fulfill];
+  //     }];
+  //    [self waitForExpectationsWithTimeout:5.0 handler:nil];
 }
 
 @end
