58f4f8f392389c7e2e95bd63e4d538b99362a5d0
Martin Xiao <martin.xiao@pactera.com.au>
2015-05-21 10:27:17 +1000

    Update PPLVoteData path

---
+++ PacteraPulse/PPLVoteDetail/Model/PPLVoteData.h
@@ -0,0 +1,22 @@
+//
+//  PPLVoteData.h
+//  PacteraPulse
+//
+//  Created by Qazi.
+//  Copyright (c) 2015 Pactera. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "PPLUtils.h"
+@interface PPLVoteData : NSObject
+@property(nonatomic, copy) NSString *userID;
+@property(nonatomic, copy) NSString *firstName;
+@property(nonatomic, copy) NSString *lastName;
+@property(nonatomic, copy) NSString *email;
+@property(nonatomic, copy) NSString *deviceID;
+@property(nonatomic, assign) FeedBackType feedBackType;
+@property(nonatomic, copy) NSArray *comments;
++ (PPLVoteData *)shareInstance;
+- (void)sendFeedback:(void (^)(BOOL status, NSString *serverResponse,
+                               NSError *error))callback;
+@end
